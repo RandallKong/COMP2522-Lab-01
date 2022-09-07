447a38eef7a9cbd9a743db52d0fb3b83682ec51c
Randall Kong <rkong360@hotmail.com>
2022-09-06 20:13:29 -0700

    Gradle Project created and unique message added on main.java

---
+++ src/main/java/org/comp2522/Main.java
@@ -0,0 +1,9 @@
+package org.comp2522;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("The San Antonio Spurs are my favorite basketball team!");
+    }
+
+}
